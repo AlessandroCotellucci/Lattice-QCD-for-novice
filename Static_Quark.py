e385993bca8e7ac157c80c16d8ace667d55c95ef
Alessandro Cotellucci <48658759+AlessandroCotellucci@users.noreply.github.com>
2019-07-03 17:57:22 +0200

    Update Static_Quark.py

--- Static_Quark.py
+++ Static_Quark.py
@@ -5,6 +5,7 @@ import cmath
 import random
 from numpy import *
 
+
 #Function that compute the dagger of a matrix
 def dagger(M):
     N=len(M)
@@ -211,15 +212,15 @@ def ProductU(U,x,y,z,t,n,f):
     productU=I.copy()  #inizializing ProductU
     for i in range(n):
         if f==3: #time case
-            productU=dot(productU,U[x,y,z,(t+i)%N,3])
+            productU=dot(productU,U[x,y,z,(t+i)%N,3].copy())
         if f==0: #space 1 case
-            productU=dot(productU,U[(x+i)%N,y,z,t,0])
+            productU=dot(productU,U[(x+i)%N,y,z,t,0].copy())
         if f==1: #space 2 case
-            productU=dot(productU,U[x,(y+i)%N,z,t,1])
+            productU=dot(productU,U[x,(y+i)%N,z,t,1].copy())
         if f==2: #space 3 case
-            productU=dot(productU,U[x,y,(z+i)%N,t,2])
+            productU=dot(productU,U[x,y,(z+i)%N,t,2].copy())
 
-    return productU
+    return productU.copy()
 
 #Function that compute the inverse side of a Wilson loop
 #input:-x,y,z,t: position in which gamma is computed
@@ -233,15 +234,15 @@ def ProductUdagger(U,x,y,z,t,n,f):
     productUdagger=I.copy() #inizializing ProductUdagger
     for i in range(n):
         if f==3: #time case
-            productUdagger=dot(productUdagger,dagger(U[x,y,z,(t-i-1)%N,3]))
+            productUdagger=dot(productUdagger,dagger(U[x,y,z,(t-i-1)%N,3].copy()))
         if f==0: #space 1 case
-            productUdagger=dot(productUdagger,dagger(U[(x-i-1)%N,y,z,t,0]))
+            productUdagger=dot(productUdagger,dagger(U[(x-i-1)%N,y,z,t,0].copy()))
         if f==1: #space 2 case
-            productUdagger=dot(productUdagger,dagger(U[x,(y-i-1)%N,z,t,1]))
+            productUdagger=dot(productUdagger,dagger(U[x,(y-i-1)%N,z,t,1].copy()))
         if f==2: #space 3 case
-            productUdagger=dot(productUdagger,dagger(U[x,y,(z-i-1)%N,t,2]))
+            productUdagger=dot(productUdagger,dagger(U[x,y,(z-i-1)%N,t,2].copy()))
 
-    return productUdagger
+    return productUdagger.copy()
 
 #Function that compute the Wilson Loop time*axradius*a mean value on the lattice using the link variables
 #generated using the Metropolis algoritm
@@ -250,83 +251,22 @@ def ProductUdagger(U,x,y,z,t,n,f):
 #inner parameters:-N:points in the lattice
 def compute_WL(U,time,radius):
     N=8
-    WL = 0.
     productUt=zeros((3,3),dtype=complex) #allocation
     productUr=zeros((3,3),dtype=complex) #allocation
     productUtdagger=zeros((3,3),dtype=complex) #allocation
     productUrdagger=zeros((3,3),dtype=complex) #allocation
-    for x in range(0,N):
-        for y in range(0,N):
-            for z in range(0,N):
-                for t in range(0,N):
+    WL=0.
+    for y in range(N):
+        for z in range(N):
+            for t in range(N-time):
+                for x in range(N-radius):
                     productUt=ProductU(U,x,y,z,t,time,3)
                     productUr=ProductU(U,x,y,z,(t+time)%N,radius,0)
                     productUtdagger=ProductUdagger(U,(x+radius)%N,y,z,(t+time)%N,time,3)
                     productUrdagger=ProductUdagger(U,(x+radius)%N,y,z,t,radius,0)
-                    WL=WL+1./3*real(trace(dot(dot(productUt,productUr),dot(productUtdagger,productUrdagger))))
+                    WL=WL+real(trace(dot(dot(productUt.copy(),productUr.copy()),dot(productUtdagger.copy(),productUrdagger.copy()))))
 
-    return WL/N**4
-
-#Function that compute the sum of all the covariant derivatives in all the directions
-#input:-U: array of link variables
-#      -x,y,z,t: point of the lattice
-#      -mi: direction
-#output:-Gauge_der: gauge covariant derivative in the point
-def Gauge_cov_deriv(U,mi,x,y,z,t):
-    u0=0.797
-    a=0.25
-    N=8
-    Gauge_der=0.
-    incmi=zeros((4),'int')
-    incro=zeros((4),'int')
-    incmi[mi]=1 #increment in the mi direction
-    #next site on mi
-    xpmi=(x+incmi[0].copy())%N
-    ypmi=(y+incmi[1].copy())%N
-    zpmi=(z+incmi[2].copy())%N
-    tpmi=(t+incmi[3].copy())%N
-    for ro in range(0,4):
-        incro[ro]=1 #increment in the ni direction
-        #next site on ro
-        xpro=(x+incro[0].copy())%N
-        ypro=(y+incro[1].copy())%N
-        zpro=(z+incro[2].copy())%N
-        tpro=(t+incro[3].copy())%N
-        #Previous site on ro
-        xmro=(x-incro[0].copy())%N
-        ymro=(y-incro[1].copy())%N
-        zmro=(z-incro[2].copy())%N
-        tmro=(t-incro[3].copy())%N
-        #next site on mi and previous on ni
-        xpmimro=(x+incmi[0].copy()-incro[0].copy())%N
-        ypmimro=(y+incmi[1].copy()-incro[1].copy())%N
-        zpmimro=(z+incmi[2].copy()-incro[2].copy())%N
-        tpmimro=(t+incmi[3].copy()-incro[3].copy())%N
-        incro[ro]=0
-        Gauge_der=Gauge_der+1./(u0**2*a**2)(dot(dot(U[x,y,z,t,ro],U[xpro,ypro,zpro,tpro,mi]),dagger(U[xpmi,ypmi,zpmi,tpmi,ro]))) \
-                            -2./(a**2)(U[x,y,z,t,mi]) \
-                                +1./(u0**2*a**2)(dot(dot(dagger(U[xmro,ymro,zmro,tmro,ro]),U[xmro,ymro,zmro,tmro,mi]),U[xpmimro,ypmimro,zpmimro,tpmimro,ro]))
-    return Gauge_der.copy()
-
-#Function that compute the substitution to suppress the high-momentum gluons
-#input:-U: array of the link variables
-#      -mi: direction
-#inner parameters:-eps: substitution term
-#                 -a: measure of the discretization of the lattice
-#output:-U: the array with the substitution in the direction mi
-def SubstitutionU(U,mi):
-    eps=1./12.
-    a=0.25
-    N=8
-    for x in range(N):
-        for y in range(N):
-            for z in range(N):
-                for t in range(N):
-                    sec_der=Gauge_cov_deriv(U,mi,x,y,z,t)
-                    sec_der2=Gauge_cov_deriv(sec_der,mi,x,y,z,t)
-                    sec_der3=Gauge_cov_deriv(sec_der2,mi,x,y,z,t)
-                    U[x,y,z,t,mi]=U[x,y,z,t,mi].copy()+4.eps*a**2*sec_der.copy()+6.eps**2*a**4*sec_der2.copy()+4.eps**3*a*6*sec_der3.copy()+eps**4*a**8*Gauge_cov_deriv(sec_der3,mi,x,y,z,t)
-    return U
+    return WL/3./N**2/(N-radius)/(N-time)
 
 
 
@@ -336,7 +276,7 @@ a=0.25
 N=8
 Nmatrix=200
 N_cf=10
-N_cor=20
+N_cor=50
 U=zeros((N,N,N,N,4,3,3),dtype=complex) #inizializing U
 WL=ones((N_cf,N,N), 'double') #inizializing WL
 M=zeros((Nmatrix,3,3),dtype=complex) #inizializing the random matrix
@@ -358,13 +298,12 @@ for x in range(0,N):
 M=randommatrixSU3(M)
 #Computation of the Monte Carlo mean value for the propagator in every time
 #in the lattice
-for j in range(0,5*N_cor): # thermalize U
+for j in range(0,2*N_cor): # thermalize U
     update(U,M)
 print('Termalized')
 for alpha in range(0,N_cf): # loop on random paths
     for j in range(0,N_cor):
         update(U,M)
-    U=SubstitutionU(U,0) #substitution in the radius direction
     WL[alpha]=0.
     for time in range(1,N):
         for radius in range(1,N):
@@ -375,18 +314,18 @@ for time in range(1,N): #computation of the Wilson loop mean value for every pos
         avg_WL[time,radius]=0.
         avg_WLSQ[time,radius]=0.
         for alpha in range(0,N_cf): # compute MC averages
-            avg_WL[time,radius]= avg_WL[time,radius]+WL[alpha,time,radius]
-            avg_WLSQ[time,radius]=avg_WLSQ[time,radius]+WL[alpha,time,radius]**2
+            avg_WL[time,radius]= avg_WL[time,radius]+WL[alpha,time,radius].copy()
+            avg_WLSQ[time,radius]=avg_WLSQ[time,radius]+WL[alpha,time,radius].copy()**2
         avg_WL[time,radius] = avg_WL[time,radius]/N_cf  #mean value on every configuration
         avg_WLSQ[time,radius] = avg_WLSQ[time,radius]/N_cf
         err_avg_WL[time,radius]=(abs(avg_WLSQ[time,radius]-avg_WL[time,radius]**2)/N_cf)**(1/2) #statistical error
 
 for i in range(0,N-1): #high time limit
     radius=i+1
-    potential[i]=abs(log(abs(avg_WL[N-2,radius]/avg_WL[N-1,radius]))) #computation of the potential
-    err_potential[i]=((1./avg_WL[N-2,radius]*err_avg_WL[N-2,radius])**2+(1./avg_WL[N-1,radius]*err_avg_WL[N-1,radius])**2)**(1/2) #propagation of the error on the potential
+    potential[i]=avg_WL[N-2,radius]/avg_WL[N-1,radius] #computation of the potential
+    err_potential[i]=((err_avg_WL[N-2,radius]/avg_WL[N-2,radius])**2+(err_avg_WL[N-1,radius]/avg_WL[N-1,radius])**2)**(1/2)*potential[i] #propagation of the error on the potential
     rad[i]=radius
-
+print(potential,err_potential)
 #plot of the numerical solution
 plt.errorbar(rad, potential, yerr=err_potential, fmt='.', color='black', label='Numerical');
 #plt.axis([0,5,0,3])
