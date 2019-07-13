1cb4d8e9e1ab49719bc11d5029e20547b61b6834
Alessandro Cotellucci <48658759+AlessandroCotellucci@users.noreply.github.com>
2019-07-13 19:05:27 +0200

    Update Static_Quark.py

--- Static_Quark.py
+++ Static_Quark.py
@@ -17,7 +17,7 @@ def dagger(M):
 #Function to compute the non liear regression
 #input:-x:parameters of the function
 #      -r:radial variables
-#      -y:value on which we want to compute the fit
+#      -y:varialbes on which we want to compute the fit
 def exact(x,r,y):
     return x[0]*r-x[1]/r+x[2]-y
 
@@ -28,7 +28,6 @@ def exact(x,r,y):
 #      -c:constant factor
 def gen_data(r,a,b,c):
     return a*r-b/r+c
-
 
 #Function that generate Nmatrix casual matrix belonging to the SU(3) group
 #input:-M:Matrix array
@@ -53,7 +52,6 @@ def randommatrixSU3(M):
 
         M[s+Nmatrix]=dagger(M[s]) #Saving the inverse
     return M.copy()
-
 
 #Function that upload the position using a metropolis algoritm considering the decrising
 #in the action using the Wilson action for QCD
@@ -83,7 +81,6 @@ def update(U,M):
                                 dS = -beta/(3)*real(trace(dot((dot(M[s].copy(),U[x,y,z,t,mi].copy())-U[x,y,z,t,mi].copy()),gamma.copy()))) # change in action
                             if dS<0 or exp(-dS)>random.uniform(0,1):
                                 U[x,y,z,t,mi] = dot(M[s].copy(),U[x,y,z,t,mi].copy())  # update U
-
 
 #Function that compute gamma for QCD using the Wilson action
 #input:-x,y,z,t: position in which gamma is computed
@@ -130,8 +127,8 @@ def Gamma(U,ro,x,y,z,t):
 
     return gamma.copy()
 
-
 #Function that compute gamma for QCD using the improved action using the rctangle terms
+#have been considered all the rectagle involvin the link variable
 #input:-x,y,z,t: position in which gamma is computed
 #      -U:array of link variables
 #      -mi:direction in which Gamma_improved is computed
@@ -221,7 +218,6 @@ def Gamma_improved(U,mi,x,y,z,t):
                                 +dot(dot(dagger(U[xp_mi_m_ni,yp_mi_m_ni,zp_mi_m_ni,tp_mi_m_ni,ni]),dagger(U[xm_ni,ym_ni,zm_ni,tm_ni,mi])),dot(dagger(U[xm_mi_m_ni,ym_mi_m_ni,zm_mi_m_ni,tm_mi_m_ni,mi]),dot(U[xm_mi_m_ni,ym_mi_m_ni,zm_mi_m_ni,tm_mi_m_ni,ni],U[xm_mi,ym_mi,zm_mi,tm_mi,mi])))
     return gamma_imp.copy()
 
-
 #Function that compute the side of a Wilson loop
 #input:-x,y,z,t: position in which gamma is computed
 #      -U:array of link variables
@@ -267,19 +263,21 @@ def ProductUdagger(U,x,y,z,t,n,f):
 #input:-U: array of the link variables
 #      -time: temporal dimension of the loop
 #      -radius: spatial dimension of the loop
-#output:-WL: mean value of the Wilson loop on all the spatial direction and all the points of the lattice
+#output:-WL: mean value of the Wilson loop on all the spatial direction and all the points of the lattice and in all the orientation
 #inner parameters:-N:points in the lattice
 def compute_WL(U,time,radius):
     N=8
-    #U=zeros((N,N,N,N,4,3,3),dtype=complex)
     productUt=zeros((3,3),dtype=complex) #allocation
     productUr=zeros((3,3),dtype=complex) #allocation
-    productUtdagger=zeros((3,3),dtype=complex) #allocation
-    productUrdagger=zeros((3,3),dtype=complex) #allocation
+    productUt_dagger=zeros((3,3),dtype=complex) #allocation
+    productUr_dagger=zeros((3,3),dtype=complex) #allocation
+    productUt_inv=zeros((3,3),dtype=complex) #allocation
+    productUr_inv=zeros((3,3),dtype=complex) #allocation
+    productUt_dagger_inv=zeros((3,3),dtype=complex) #allocation
+    productUr_dagger_inv=zeros((3,3),dtype=complex) #allocation
     inc=zeros((3),'int') #allocation
     WL=0.
     for space in range(3):
-        #U=SubstitutionU(F,space) #Substitution with the smeared quantity
         inc[space]=1 #increment in the spatial direction
         for x in range(N):
             for y in range(N):
@@ -289,13 +287,90 @@ def compute_WL(U,time,radius):
                         x_inc=(x+radius*inc[0])%N
                         y_inc=(y+radius*inc[1])%N
                         z_inc=(z+radius*inc[2])%N
+                        #Wilson line in the counterclockwise direction
                         productUt=ProductU(U,x,y,z,t,time,3) #Wilson line in time direction
                         productUr=ProductU(U,x,y,z,(t+time)%N,radius,space) #Wilson line in space direction
-                        productUtdagger=ProductUdagger(U,x_inc,y_inc,z_inc,(t+time)%N,time,3) #inverse Wilson line in time direction
-                        productUrdagger=ProductUdagger(U,x_inc,y_inc,z_inc,t,radius,space) #inverse Wilson line in space direction
-                        WL=WL+real(trace(dot(productUt,dot(productUr,dot(productUtdagger,productUrdagger))))) #compute of the Wilson loop
+                        productUt_dagger=ProductUdagger(U,x_inc,y_inc,z_inc,(t+time)%N,time,3) #inverse Wilson line in time direction
+                        productUr_dagger=ProductUdagger(U,x_inc,y_inc,z_inc,t,radius,space) #inverse Wilson line in space direction
+                        #Wilson line in the clockwise direction
+                        productUt_inv=ProductU(U,x_inc,y_inc,z_inc,t,time,3)
+                        productUr_inv=ProductU(U,x,y,z,t,radius,space)
+                        productUr_dagger_inv=ProductUdagger(U,x_inc,y_inc,z_inc,(t+time)%N,radius,space)
+                        productUt_dagger_inv=ProductUdagger(U,x,y,z,(t+time)%N,time,3)
+                        #Computation of the Wilson loop in the two possible directions
+                        WL=WL+real(trace(dot(productUt,dot(productUr,dot(productUt_dagger,productUr_dagger))))) \
+                                +real(trace(dot(productUr_inv,dot(productUt_inv,dot(productUr_dagger_inv,productUt_dagger_inv)))))
         inc[space]=0
-    return abs(WL)/9./N**4
+    return WL/9./N**4/2
+
+#Function that compute the substitution to suppress the high-momentum gluons
+#input:-U: array of the link variables
+#inner parameters:-eps: substitution term
+#                 -a: measure of the discretization of the lattice
+#output:-U: the array with the substitution in the direction mi
+def SubstitutionU(U):
+    eps=1./12.
+    a=0.25
+    N=8
+    sec_der=zeros((N,N,N,N,4,3,3),dtype=complex) #allocation
+    sec_der=Gauge_cov_deriv(U) #computes of the second order derivative
+
+    for x in range(N):
+        for y in range(N):
+            for z in range(N):
+                for t in range(N):
+                    for mi in range(0,3):
+                        U[x,y,z,t,mi]=U[x,y,z,t,mi].copy()+eps*a**2*sec_der[x,y,z,t,mi].copy()
+    return U
+
+#Function that compute the discretized gauge covariant derivative for every points and sum in all the directions
+#input:-U:link variables array of which we want to compute the derivative
+#inner parameters:-u0: average value of the link variable
+#                 -a: measure of the dicretization of the lattice
+#                 -N: number of points in the lattice for side
+#output:-Gauge_der: array containing the gauge derivative on the lattice
+def Gauge_cov_deriv(U):
+    u0=0.797
+    a=0.25
+    N=8
+    Gauge_der=zeros((N,N,N,N,4,3,3),dtype=complex)
+    incmi=zeros((4),'int')
+    incro=zeros((4),'int')
+
+    for x in range(N):
+        for y in range(N):
+            for z in range(N):
+                for t in range(N):
+                    for mi in range(0,4):
+                        incmi[mi]=1 #increment in the mi direction
+                        #next site on mi
+                        xp_mi=(x+incmi[0].copy())%N
+                        yp_mi=(y+incmi[1].copy())%N
+                        zp_mi=(z+incmi[2].copy())%N
+                        tp_mi=(t+incmi[3].copy())%N
+                        for ro in range(0,4):
+                            incro[ro]=1 #increment in the ni direction
+                            #next site on ro
+                            xp_ro=(x+incro[0].copy())%N
+                            yp_ro=(y+incro[1].copy())%N
+                            zp_ro=(z+incro[2].copy())%N
+                            tp_ro=(t+incro[3].copy())%N
+                            #Previous site on ro
+                            xm_ro=(x-incro[0].copy())%N
+                            ym_ro=(y-incro[1].copy())%N
+                            zm_ro=(z-incro[2].copy())%N
+                            tm_ro=(t-incro[3].copy())%N
+                            #next site on mi and previous on ni
+                            xp_mi_m_ro=(x+incmi[0].copy()-incro[0].copy())%N
+                            yp_mi_m_ro=(y+incmi[1].copy()-incro[1].copy())%N
+                            zp_mi_m_ro=(z+incmi[2].copy()-incro[2].copy())%N
+                            tp_mi_m_ro=(t+incmi[3].copy()-incro[3].copy())%N
+                            incro[ro]=0
+                            Gauge_der[x,y,z,t,mi]=Gauge_der[x,y,z,t,mi]+1./(u0**2*a**2)*(dot(dot(U[x,y,z,t,ro],U[xp_ro,yp_ro,zp_ro,tp_ro,mi]),dagger(U[xp_mi,yp_mi,zp_mi,tp_mi,ro]))) \
+                                        -2./(a**2)*(U[x,y,z,t,mi].copy()) \
+                                        +1./(u0**2*a**2)*(dot(dot(dagger(U[xm_ro,ym_ro,zm_ro,tm_ro,ro]),U[xm_ro,ym_ro,zm_ro,tm_ro,mi]),U[xp_mi_m_ro,yp_mi_m_ro,zp_mi_m_ro,tp_mi_m_ro,ro]))
+                        incmi[mi]=0
+    return Gauge_der.copy()
 
 
 
@@ -303,20 +378,21 @@ def compute_WL(U,time,radius):
 #allocation of the arrays and definition of the parameters
 a=0.25 #spacing of the lattice
 N=8 #number of site in the lattice
-N_sim=5
-N_sim_t=8
+N_sim_r=4 #maximal extension of the Wilson loop in the radial direction +1
+N_sim_t=5 #maximal extension of Wilson loop in the temporal direction +1
 Nmatrix=200 #total number of SU(3) matrices
 N_cf=10 #number of montecarlo configuration
 N_cor=50 #number of update before compute the result
 U=zeros((N,N,N,N,4,3,3),dtype=complex) #inizializing U: link variables
-WL=zeros((N_cf,N_sim_t,N_sim), 'double') #inizializing WL: Wilson loop rsults for every configuration
+F=zeros((N,N,N,N,4,3,3),dtype=complex)
+WL=zeros((N_cf,N_sim_t,N_sim_r), 'double') #inizializing WL: Wilson loop rsults for every configuration
 M=zeros((Nmatrix,3,3),dtype=complex) #inizializing the random matrix
-avg_WL=zeros((N_sim_t,N_sim),'double') #inizializing avg_WL: mean value of the Wilson loop on the different configuration
-avg_WLSQ=zeros((N_sim_t,N_sim),'double') #inizializing avg_WLSQ: mean value of the Wilson loop squared on the different configuration
-err_avg_WL=zeros((N_sim_t,N_sim),'double') #inizializing err_avg_WL: statistical error of avg_WL
-potential=zeros(N_sim-1) #inizializing the potential
-err_potential=zeros(N_sim-1) #inizializing the err_potential
-rad=zeros(N_sim-1) #inizializing the radius
+avg_WL=zeros((N_sim_t,N_sim_r),'double') #inizializing avg_WL: mean value of the Wilson loop on the different configuration
+avg_WLSQ=zeros((N_sim_t,N_sim_r),'double') #inizializing avg_WLSQ: mean value of the Wilson loop squared on the different configuration
+err_avg_WL=zeros((N_sim_t,N_sim_r),'double') #inizializing err_avg_WL: statistical error of avg_WL
+potential=zeros(N_sim_r-1) #inizializing the potential
+err_potential=zeros(N_sim_r-1) #inizializing the err_potential
+rad=zeros(N_sim_r-1) #inizializing the radius
 # inizializing U with the identity which belongs to the SU(3) group
 for x in range(0,N):
     for y in range(0,N):
@@ -335,39 +411,41 @@ print('Termalized')
 for alpha in range(0,N_cf): # loop on random paths
     for j in range(0,N_cor):
         update(U,M)
-
+    F=U.copy()
+    F=SubstitutionU(F) #substitution with the smeared quantity
     for time in range(1,N_sim_t):
-        for radius in range(1,N_sim):
-            WL[alpha,time,radius]=compute_WL(U,time,radius) #compute WL
+        for radius in range(1,N_sim_r):
+            WL[alpha,time,radius]=compute_WL(F,time,radius) #compute WL
 
     print("End simulation number:",alpha+1)
 
 for time in range(1,N_sim_t): #computation of the Wilson loop mean value for every possible values of time and radius
-    for radius in range(1,N_sim):
+    for radius in range(1,N_sim_r):
         avg_WL[time,radius]=0.
         avg_WLSQ[time,radius]=0.
         for alpha in range(0,N_cf): # compute MC averages
             avg_WL[time,radius]= avg_WL[time,radius]+WL[alpha,time,radius]
             avg_WLSQ[time,radius]=avg_WLSQ[time,radius]+WL[alpha,time,radius]**2
         avg_WL[time,radius] = avg_WL[time,radius]/N_cf  #mean value on every configuration
-        avg_WLSQ[time,radius] = avg_WLSQ[time,radius]/N_cf
+        avg_WLSQ[time,radius] = avg_WLSQ[time,radius]/N_cf #mean value of the square on every configuration
         err_avg_WL[time,radius]=((avg_WLSQ[time,radius]-avg_WL[time,radius]**2)/N_cf)**(1/2) #statistical error
 
-for i in range(0,N_sim-1): #computation of the fraction in the high time limit with the error
+for i in range(0,N_sim_r-1): #computation of the fraction in the high time limit with the error
     radius=i+1
     potential[i]=avg_WL[N_sim_t-2,radius]/avg_WL[N_sim_t-1,radius] #computation of the potential
-    err_potential[i]=((err_avg_WL[N_sim-1,radius]/avg_WL[N_sim-1,radius])**2+(err_avg_WL[N_sim-2,radius]/avg_WL[N_sim-2,radius])**2)**(1/2)*potential[i] #propagation of the error on the potential
+    err_potential[i]=((err_avg_WL[N_sim_t-1,radius]/avg_WL[N_sim_t-1,radius])**2+(err_avg_WL[N_sim_t-2,radius]/avg_WL[N_sim_t-2,radius])**2)**(1/2)*potential[i] #propagation of the error on the potential from the wilson loop estimation
     rad[i]=radius
 
 #non linear regression using the fit function
-x=ones(3)
-res_lsq = least_squares(exact, x, args=(rad, potential))
-rad_test = linspace(1, 5,100)
-V_lsq = gen_data(rad_test, *res_lsq.x)
+x=ones(3) #array for the conctants of the fit
+res_lsq = least_squares(exact, x, args=(rad, potential)) #application of the least square method using the scipy function
+rad_test = linspace(0.5, 4,100) #generation of points for the graph
+V_lsq = gen_data(rad_test, *res_lsq.x) #generation of points for the graph
 
 #plot of the numerical solution and the fit curve
 plt.errorbar(rad, potential, yerr=err_potential, fmt='.', color='blue', label='Numerical');
 plt.plot(rad_test,V_lsq,'b',label='Regression')
+plt.axis([0.5,4,1,6.5])
 plt.legend(loc='upper right')
 plt.title('Static quark potential')
 plt.xlabel('$r/a$')
